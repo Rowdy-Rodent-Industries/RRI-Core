0ce017a3bbf12ebb257cd3b40460d7f9da0656cb
Rat <markreigle52@gmail.com>
2026-05-25 19:09:22 -0400

    fixed an mti class

--- Addons/main/script_macros_armoury.hpp
+++ Addons/main/script_macros_armoury.hpp
@@ -1,4 +1,4 @@
-#define COREPREFIX mti
+#define COREPREFIX rri
 #define IS_CORE_LOADED (GVARMAIN(coreLoaded))
 
 #define GVAR_CORE(var1,var2) TRIPLES(COREPREFIX,var1,var2)
@@ -7,7 +7,7 @@
 
 #define FUNC_CORE_SYS(var1,var2) TRIPLES(DOUBLES(COREPREFIX,var1),fnc,var2)
 
-// FUNC_CORE(var1,var2,var3) checks if mti core is loaded and returns a default value var3 if the function mti_var1_fnc_var2 is not available
+// FUNC_CORE(var1,var2,var3) checks if rri core is loaded and returns a default value var3 if the function rri_var1_fnc_var2 is not available
 #define FUNC_CORE(var1,var2,var3) (if (IS_CORE_LOADED) then { FUNC_CORE_SYS(var1,var2)} else { (compile QUOTE(var3)) })
 #define C_FUNC_CORE(var1,var2,var3) (if (IS_CORE_LOADED) then { FUNC_CORE_SYS(var1,var2)} else { (compile QUOTE(QUOTE(var3))) })
 #define QFUNC_CORE(var1,var2) QUOTE(FUNC_CORE_SYS(var1,var2))
